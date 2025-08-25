52cc054ac9e72888fb277a64f18f8861ae2b3689
Apple <apple@MacBookPro.lan>
2025-08-25 22:53:35 +0530

    logs

--- src/options.rs
+++ src/options.rs
@@ -218,6 +218,7 @@ impl Options {
 
     let ord_chain = self.chain();
 
+    log::info!("rpc chain {:#?} | ord_chain {:#?}",rpc_chain,ord_chain);
     if rpc_chain != ord_chain {
       bail!("Dogecoin RPC server is on {rpc_chain} but ord is on {ord_chain}");
     }
